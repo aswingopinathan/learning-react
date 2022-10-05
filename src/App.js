598199eb0a78169fc8dac35028d931e8883da810
Aswin Gopinathan <aswingopinathan@gmail.com>
2022-10-05 15:06:51 +0530

    data update firebase

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ function App() {
     <div>
      <h1>HELLO WORLD</h1>
      <button onClick={()=>{
-     Firebase.firestore().collection('Products').get().then(snapshot=>{
-      snapshot.forEach((obj)=>{
-        console.log(obj.data());
-      })
+     Firebase.firestore().collection('Products').doc('p471pb26t9QWYGbl9BPn').set({
+      price:'6000'
+     }).then(()=>{
+      console.log("Data modified");
      })
      }}>Click Me</button>
     </div>
